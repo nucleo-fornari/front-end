c5faa5dbcf6fcabe46de7884b1db95d777a8a174
Conde <157609877+fvconde@users.noreply.github.com>
2024-10-14 14:21:35 -0300

    Revert "site institucional"
    This reverts commit d44286adf6c4ce0c6834a8694d4dd9aaa75f9fc8, reversing
    changes made to 3dd8f57b680bccc3114c770a41eb37a77ffc1ea3.

--- nucleo-fornari/frontend/tailwind.config.js
+++ nucleo-fornari/frontend/tailwind.config.js
@@ -23,26 +23,20 @@ module.exports = {
       },
       white: {
         main: '#edebeb',
-        cinzabg: '#f2f0f0'
+        gray: '#f2f0f0'
       },
       black: {
         main: '#141414'
       }     
     },
-
     extend: {
+      height: {
+        '10':'10%',
+        '67':'67px'
+      },
       width:{
-        '100': '100px',
-        '500':'500px'
-
-      },
-      height:{
-        '67': '67px',
-        '400':'400px'
-      },
-      backgroundImage: {
-        'radial-custom': 'linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgba(130, 170, 229, 1) 35%, rgba(130, 170, 229, 1) 68%, rgba(255, 255, 255, 1) 100%)',
-      },
+        '100' : '100px'
+      }
     },
   },
   plugins: [],
