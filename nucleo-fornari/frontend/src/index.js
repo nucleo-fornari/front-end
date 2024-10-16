594facd00fc6af406af917e5a476adbf61e290ec
Carlos Eduardo <carlos.quinteros@sptech.school>
2024-10-16 11:11:50 -0300

    FEAT: Agenda de eventos.

--- nucleo-fornari/frontend/src/index.js
+++ nucleo-fornari/frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // import ProfessorPage from './views/professor/professorView';
 import './index.css'
 // import Formulario from './components/Login/Formulario';
 // import ParentsPage from './views/parents/parentsView';
 // import SecretaryPage from './views/secretary/secretaryView'
-import SideMenu from './components/SideMenu/sideMenuView';
-import ParentsPage from './views/parents/parentsView';
+// import SideMenu from './components/SideMenu/sideMenuView';
+// import ParentsPage from './views/parents/parentsView';
+import Calendar from './components/Calendar/Calendar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ParentsPage></ParentsPage>
+    <LocalizationProvider adapterLocale="pt-br" dateAdapter={AdapterDayjs}>
+    <Calendar></Calendar>
+    </LocalizationProvider>
   </React.StrictMode>
 );
