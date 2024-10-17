5556e1a480b1447aac3a03238bf2470a0a6d2779
Carlos Eduardo <carlos.quinteros@sptech.school>
2024-10-17 17:01:44 -0300

    FIX: SideMenu e Rota Atualizadas

--- nucleo-fornari/frontend/src/components/SideMenu/SideMenuProfessor.js
+++ nucleo-fornari/frontend/src/components/SideMenu/SideMenuProfessor.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SideMenuItem from './SideMenuItem';
-import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
-import StickyNote2RoundedIcon from '@mui/icons-material/StickyNote2Rounded';
 import NewspaperRoundedIcon from '@mui/icons-material/NewspaperRounded';
+import InboxIcon from '@mui/icons-material/Inbox';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import DescriptionIcon from '@mui/icons-material/Description';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 // import { FaRegNewspaper, FaCalendarAlt, FaUsers } from 'react-icons/fa';
 
 const SideMenu = () => {
@@ -13,26 +15,27 @@ const SideMenu = () => {
         setIsExpanded(!isExpanded); // Alterna entre expandido e contraído
     };
 
-    // const menuItems = [
-    //     { icon: '', text: 'Publicações', route: '/publicacoes' },
-    //     // { icon: <FaUsers className="text-2xl" />, text: 'Reuniões', route: '/reunioes' },
-    //     // { icon: <FaCalendarAlt className="text-2xl" />, text: 'Agenda', route: '/agenda' }
-    // ];
+    const menuItems = [
+        { icon: '', text: 'Publicações', route: '/publicacoes' },
+        // { icon: <FaUsers className="text-2xl" />, text: 'Reuniões', route: '/reunioes' },
+        // { icon: <FaCalendarAlt className="text-2xl" />, text: 'Agenda', route: '/agenda' }
+    ];
 
     return (
         <div className={`h-screen bg-white-main  ${isExpanded ? 'w-64' : 'w-20'} transition-width duration-300 left-0 top-0 flex flex-col items-center`}>
 
             <div className="my-4 flex flex-col items-center mb-20">
-                <div className='flex flex-row'>
+                <div className="flex items-center justify-between w-full">
 
-                    <img
-                        src=""
-                        alt="User/Project"
-                        className={`rounded-full ${isExpanded ? 'w-24 h-24' : 'w-10 h-10'} transition-all duration-300`}
-                    />
+                    {isExpanded && (
+                        <div className="flex-grow flex justify-center">
+                            <AccountCircleIcon sx={{ fontSize: 100 }} />
+                        </div>
+                    )}
+
                     <button
                         onClick={toggleMenu}
-                        className="mb-4 p-2 text-white bg-blue-pastel hover:bg-blue-main rounded-full transition-colors h-10"
+                        className="ml-2 mb-4 p-2 text-white bg-blue-pastel hover:bg-blue-main rounded-full transition-colors h-10 w-10 flex items-center justify-center"
                     >
                         {isExpanded ? '<' : '>'}
                     </button>
@@ -40,6 +43,7 @@ const SideMenu = () => {
 
                 {isExpanded && <h2 className="text-white mt-2">Nome do Usuário</h2>}
             </div>
+
 
             {/* <nav className="mt-10 flex-grow flex flex-col justify-center items-center w-full">
                 {menuItems.map((item, index) => (
@@ -52,27 +56,33 @@ const SideMenu = () => {
                 ))}
             </nav> */}
 
-            {/* Lista de botões de navegação */}
-            <Link to="/responsavel">
+            {isExpanded && <Link to="/professor">
                 <SideMenuItem
                     titulo="Publicações"
                     icone={<NewspaperRoundedIcon />}
                 />
-            </Link>
+            </Link>}
 
-            <Link to="/responsavel/reunioes">
+            {isExpanded && <Link to="/responsavel/reunioes">
                 <SideMenuItem
-                    titulo="Reuniões"
-                    icone={<PeopleRoundedIcon />}
+                    titulo="Chamados"
+                    icone={<InboxIcon />}
                 />
-            </Link>
+            </Link>}
 
-            <Link to="/responsavel/agenda">
+            {isExpanded && <Link to="/responsavel/agenda">
                 <SideMenuItem
-                    titulo="Agenda"
-                    icone={<StickyNote2RoundedIcon />}
+                    titulo="Calendário"
+                    icone={<CalendarMonthIcon />}
                 />
-            </Link>
+            </Link>}
+
+            {isExpanded && <Link>
+                <SideMenuItem
+                    titulo="Relatórios"
+                    icone={<DescriptionIcon />}
+                />
+            </Link>}
         </div>
 
     );
