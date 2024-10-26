bf4a7021e774619b8e326dd640c0e1487647685a
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-10-26 17:02:52 -0300

    Implementado todas as rotas

---
+++ nucleo-fornari/frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import ProfessorPage from './views/professor/professorView';
+import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/Routes';
+// import Formulario from './components/Login/Formulario';
+// import ParentsPage from './views/parents/parentsView';
+// import SecretaryPage from './views/secretary/secretaryView'
+// import SideMenu from './components/SideMenu/sideMenuView';
+// import ParentsPage from './views/parents/parentsView';
+import Calendar from './components/Calendar/Calendar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- nucleo-fornari/frontend/src/views/home/home.js
+++ nucleo-fornari/frontend/src/views/home/home.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import ProfessorPage from './views/professor/professorView';
-import './index.css'
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/Routes';
-// import Formulario from './components/Login/Formulario';
-// import ParentsPage from './views/parents/parentsView';
-// import SecretaryPage from './views/secretary/secretaryView'
-import SideMenu from './components/SideMenu/sideMenuView';
-import Header from './components/Site/Header';
-import logo from './assets/imgs/logoAzul.png'
-import Banner from './components/Site/Banner';
-import ProjetoSection from './components/Site/ProjetoSection';
-import Escola from './components/Site/Escola';
-import Footer from './components/Site/Footer';
+// import './index.css'
+import Header from '../../components/Site/Header';
+import logo from '../../assets/imgs/logoAzul.png'
+import Banner from '../../components/Site/Banner';
+import ProjetoSection from '../../components/Site/ProjetoSection';
+import Escola from '../../components/Site/Escola';
+import Footer from '../../components/Site/Footer';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+function Home(){
+  return(
+    <React.StrictMode>
     <Header logoAzul={logo}/>
     <Banner />
     <ProjetoSection />
     <Escola />
     <Footer/>
   </React.StrictMode>
-);
+  );
+}
+export default Home;
