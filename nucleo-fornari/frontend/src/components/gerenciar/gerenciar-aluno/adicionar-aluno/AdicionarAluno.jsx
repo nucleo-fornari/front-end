b3da14e1b0d15474e6c66f034f3f8409f55a34d2
caiqueluci0 <caique.lucio@sptech.school>
2024-11-15 15:28:41 -0300

    merge

--- nucleo-fornari/frontend/src/components/gerenciar/gerenciar-aluno/adicionar-aluno/AdicionarAluno.jsx
+++ nucleo-fornari/frontend/src/components/gerenciar/gerenciar-aluno/adicionar-aluno/AdicionarAluno.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './adicionarAluno.css'
+import './AdicionarAluno.css'
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
 import { CheckBox } from '@mui/icons-material';
