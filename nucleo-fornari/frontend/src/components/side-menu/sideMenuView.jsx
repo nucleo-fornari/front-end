973048b3688c1261fdb046f082713214debee532
juliadmoura <julia.moura@sptech.school>
2024-11-21 09:55:52 -0300

    side menu praticamente finalizado

--- nucleo-fornari/frontend/src/components/side-menu/sideMenuView.jsx
+++ nucleo-fornari/frontend/src/components/side-menu/sideMenuView.jsx
@@ -1,79 +1,95 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./sideMenuView.css";
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton } from '@mui/material';
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
 
 const SideMenu = ({ menuItens }) => {
-    const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(true);
 
-    const toggleMenu = () => {
-        setIsExpanded(!isExpanded);
-    };
+  const toggleMenu = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <div className={
-            `h-screen flex flex-col items-center transition-all duration-700 ease-out bg-blue-dash text-white-ice ${isExpanded ? 'w-64' : 'w-20'}`}
-        >
+  return (
+    <div
+      className={`lg:h-screen lg:flex lg:flex-col lg:items-center transition-all duration-700 ease-out bg-blue-dash text-white-ice
+        lg:${isExpanded ? "w-64" : "w-20"}
+        md:w-screen
+        md:h-20
+        `}
+    >
+      <div
+        className={`flex flex-row w-full justify-center items-center 
+            lg:${isExpanded ? "w-64" : "w-20"}`}
+      >
+        <button onClick={toggleMenu} className="lg:block h-16 md:hidden">
+          {isExpanded ? (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ ml: 0 }}
+            >
+              <MenuOpenIcon sx={{ color: "white" }} />
+            </IconButton>
+          ) : (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ ml: 0 }}
+            >
+              <MenuIcon sx={{ color: "white" }} />
+            </IconButton>
+          )}
+        </button>
+      </div>
+      {isExpanded && (
+        <h2 className="lg:block md:hidden text-white my-8">
+          Olá,{" "}
+          {sessionStorage.NOME && sessionStorage.NOME.length > 3
+            ? sessionStorage.NOME
+            : "user"}
+          !{" "}
+        </h2>
+      )}
 
-            <div className='flex flex-col items-center w-full'>
-                <div class={`containner-logo ${isExpanded ? 'w-64' : 'w-20'}`}>
-                    {/* 
-                    <img
-                        src={<AccountCircleIcon/>}
-                    {/* <img
-                        src={Logo}
-                        alt="Project"
-                        class={`img-logo ${isExpanded ? 'expanded' : 'collapsed'}`}
-                    /> */}
-                    <button
-                        onClick={toggleMenu}
-                        class="btn-espande"
-                    >
-                        {isExpanded ?
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuOpenIcon sx={{ color: 'white' }} />
-                        </IconButton> : <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon sx={{ color: 'white' }} />
-                    </IconButton>}
-                    </button>
-                </div>
-                {isExpanded && <h2 className="text-white mt-10">Olá, {(sessionStorage.NOME && sessionStorage.NOME.length > 3) ? sessionStorage.NOME : "user"}! </h2>}
-            </div>
+      <hr />
 
-            <hr />
-
-            <div class="containner-btns-route">
-                {menuItens.map((item, index) => (
-                    <Link
-                        key={index}
-                        to={item.route}
-                        className={`w-full ${location.pathname === item.route ? 'bg-blue-pastel' : ''} hover:bg-blue-pastel`}
-                    >
-                        <button key={index} class="btn-route">
-                            {item.icon}
-                            {isExpanded && <span class="text-route">{item.name}</span>}
-                        </button>
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
+      <div
+        class="flex lg:flex-col lg:justify-center lg:items-center lg:w-full lg:mb-72
+      md:w-full md:justify-around"
+      >
+        {menuItens.map((item, index) => (
+          <Link
+            key={index}
+            to={item.route}
+            className={`lg:w-full md:w-20 md:h-full ${
+              location.pathname === item.route ? "bg-blue-pastel" : ""
+            } hover:bg-blue-pastel`}
+          >
+            <button
+              key={index}
+              class="flex lg:items-center lg:flex-row lg:w-full pl-8 pr-4 py-6"
+            >
+              {item.icon}
+              {isExpanded && (
+                <span class="lg:block ml-4 text-lg leading-4 font-semibold md:hidden">
+                  {item.name}
+                </span>
+              )}
+            </button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SideMenu;
