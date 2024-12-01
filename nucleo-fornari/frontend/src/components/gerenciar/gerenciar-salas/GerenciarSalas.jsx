a7d239556be1cfa736113f614b4dabcbfea107bb
Carlos Eduardo <carlos.quinteros@sptech.school>
2024-11-30 23:23:09 -0300

    FEAT: Modal para confirmar ação adicionada

--- nucleo-fornari/frontend/src/components/gerenciar/gerenciar-salas/GerenciarSalas.jsx
+++ nucleo-fornari/frontend/src/components/gerenciar/gerenciar-salas/GerenciarSalas.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box} from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import api from '../../../services/api';
 import { useNavigate, Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import ModalConfirm from '../../modals/confirmar-acao/ModalConfirm';
 
 const GerenciarSalas = () => {
-    
+
     const navigate = useNavigate();
     const [data, setData] = useState([]);
-    const [sortOption, setSortOption] = useState(0);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [salaClicked, setSalaClicked] = useState("");
+
+    const handleOpenModal = (sala) => {
+        setSalaClicked(sala)
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => setModalOpen(false);
+
+    const handleConfirmAction = () => {
+        handleDelete()
+        setModalOpen(false);
+    };
 
     const fetchChamados = () => {
         api.get('/salas')
@@ -27,12 +42,26 @@ const GerenciarSalas = () => {
         navigate('/secretaria/editar/salas', { state: { data } });
     }
 
+    const handleDelete = () => {
+            api.delete('/salas/' + parseInt(salaClicked.id))
+                .then((res) => {
+                    if (res.status === 204) {
+                        setData((prevData) => prevData.filter((sala) => sala.id !== salaClicked.id));
+                        toast.success('Sala ' + salaClicked.nome + ' deletada com sucesso!');
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                    toast.error('Erro ao deletar a sala: ' + salaClicked.nome);
+                });
+        }
+
     return (
         <div className="mt-12 flex flex-col gap-4">
             <div className="flex justify-start h-15 ml-12">
                 <Link
-                to={'/secretaria/cadastro/sala'}
-                >                
+                    to={'/secretaria/cadastro/sala'}
+                >
                     <span class='flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold'>
                         Criar sala
                     </span>
@@ -40,38 +69,48 @@ const GerenciarSalas = () => {
             </div>
             <Box sx={{ p: 5 }}>
                 {!data.length <= 0 ?
-                <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1 }}>
-                    <Table>
-                        <TableHead>
-                            <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Sala</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Localização</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Grupo</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Quantidade de Alunos</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Ações</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {
-                            data.map((sala) => (
-                                <TableRow key={sala.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
-                                    <TableCell align="center">{sala.nome}</TableCell>
-                                    <TableCell align="center">{sala.localizacao}</TableCell>
-                                    <TableCell align="center">{sala.grupo?.nome || 'Sem grupo'}</TableCell>
-                                    <TableCell align="center">{sala.alunos.length}</TableCell>
-                                    <TableCell align="center">
-                                        <EditIcon onClick={() => handleEdit(sala)} style={{ color: 'blue', cursor: 'pointer', marginRight: 8 }} />
-                                        <DeleteIcon style={{ color: 'red', cursor: 'pointer' }} />
-                                    </TableCell>
+                    <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1 }}>
+                        <Table>
+                            <TableHead>
+                                <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Sala</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Localização</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Grupo</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Quantidade de Alunos</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Quantidade de Professores</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Ações</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                :
-                <span>Sem salas cadastradas</span>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    data.map((sala) => (
+                                        <TableRow key={sala.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
+                                            <TableCell align="center">{sala.nome}</TableCell>
+                                            <TableCell align="center">{sala.localizacao}</TableCell>
+                                            <TableCell align="center">{sala.grupo?.nome || 'Sem grupo'}</TableCell>
+                                            <TableCell align="center">{sala.alunos.length}</TableCell>
+                                            <TableCell align="center">{sala.professores.length}</TableCell>
+                                            <TableCell align="center">
+                                                <EditIcon onClick={() => handleEdit(sala)} style={{ color: 'blue', cursor: 'pointer', marginRight: 8 }} />
+                                                <DeleteIcon onClick={() => handleOpenModal(sala)} style={{ color: 'red', cursor: 'pointer' }} />
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    :
+                    <span>Sem salas cadastradas</span>
                 }
             </Box>
+
+            <ModalConfirm
+                open={modalOpen}
+                onClose={handleCloseModal}
+                onConfirm={handleConfirmAction}
+                title="Deseja deletar essa sala?"
+                description={`A sala ${salaClicked.nome} será deletada, Após confirmar esta ação não poderá ser desfeita.`}
+            />
         </div>
     );
 };
--- nucleo-fornari/frontend/src/components/gerenciar/gerenciar-salas/editar-sala/EditarSala.jsx
+++ nucleo-fornari/frontend/src/components/gerenciar/gerenciar-salas/editar-sala/EditarSala.jsx
@@ -1,5 +1,7 @@
 import { useLocation } from 'react-router-dom';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from "@mui/material";
+import { Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import ModalAtrelar from '../../../modals/atrelar/ModalAtrelar';
@@ -11,6 +13,8 @@ const EditarSala = () => {
     const [dataState, setDataState] = useState(data);
     const [openModal, setOpenModal] = useState(false);
     const [role, setRole] = useState("");
+    const [newName, setNewName] = useState('');
+    const [newLocation, setNewLocation] = useState('');
 
     const handleBack = () => {
         navigate('/secretaria/gerencia/salas');
@@ -21,87 +25,129 @@ const EditarSala = () => {
         setOpenModal(true);
     }
 
+    const handleUpdate = (field) => {
+        if (field === 'nome') {
+            console.log('Novo nome:', newName);
+            // Lógica para atualizar o nome
+        } else if (field === 'localizacao') {
+            console.log('Nova localização:', newLocation);
+            // Lógica para atualizar a localização
+        }
+    };
+
+    const handleEdit = (data) => {
+        //navigate('/secretaria/editar/salas', { state: { data } });
+    }
+
+    const handleDelete = (data) => {
+        // api.delete( + parseInt(data.id))
+        //     .then((res) => {
+        //         if (res.status === 204) {
+        //             setData((prevData) => prevData.filter((sala) => sala.id !== data.id));
+        //             toast.success('Sala ' + data.nome + ' deletada com sucesso!');
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         toast.error('Erro ao deletar a sala: ' + data.nome);
+        //     });
+    }
+
     return (
         <>
-        <div className="mt-12 flex flex-col gap-4">
-            <div className="flex justify-between items-center h-15 ml-12">
-                <button
-                    className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
-                    onClick={() => handleBack()}
-                >
-                    Voltar
-                </button>
+            <div className="mt-12 flex flex-col gap-4">
+                <div className="flex justify-between items-center h-15 ml-12">
+                    <div className="flex items-center space-x-4 mr-12">
+                        <button
+                            className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
+                            onClick={() => handleBack()}
+                        >
+                            Voltar
+                        </button>
 
-                <div className="flex space-x-4 mr-12">
-                    <button
-                        className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
-                        onClick={() => handleAtrelar("PROFESSOR")}
-                    >
-                        Atrelar Professor
-                    </button>
-                    <button
-                        className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
-                        onClick={() => handleAtrelar("ALUNO")}
-                    >
-                        Atrelar Aluno
-                    </button>
+                            <Typography variant="h6">Sala: {data.nome}</Typography>
+                            <Typography variant="h6">Localização: {data.localizacao}</Typography>
+                    </div>
+
+                    <div className="flex space-x-4 mr-12">
+                        <button
+                            className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
+                            onClick={() => handleAtrelar("PROFESSOR")}
+                        >
+                            Atrelar Professor
+                        </button>
+                        <button
+                            className="flex justify-center items-center bg-blue-500 text-white-ice px-7 py-2 rounded-lg font-semibold"
+                            onClick={() => handleAtrelar("ALUNO")}
+                        >
+                            Atrelar Aluno
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <Box sx={{ p: 5 }}>
-                {/* Tabela de Professores */}
-                <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1, marginBottom: 4 }}>
-                    <h2 style={{ marginLeft: 16 }}>Professores</h2>
-                    <Table>
-                        <TableHead>
-                            <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Nome</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>CPF</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Telefone</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>E-mail</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Data de Nascimento</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {dataState.professores.map((professor) => (
-                                <TableRow key={professor.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
-                                    <TableCell align="center">{professor.nome}</TableCell>
-                                    <TableCell align="center">{professor.cpf}</TableCell>
-                                    <TableCell align="center">{professor.telefone}</TableCell>
-                                    <TableCell align="center">{professor.email}</TableCell>
-                                    <TableCell align="center">{professor.dtNasc}</TableCell>
+                <Box sx={{ p: 5 }}>
+                    {/* Tabela de Professores */}
+                    <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1, marginBottom: 4 }}>
+                        <h2 style={{ marginLeft: 16 }}>Professores</h2>
+                        <Table>
+                            <TableHead>
+                                <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Nome</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>CPF</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Telefone</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>E-mail</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Data de Nascimento</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Ações</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1 }}>
-                    <h2 style={{ marginLeft: 16 }}>Alunos</h2>
-                    <Table>
-                        <TableHead>
-                            <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>RA</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Nome</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Laudado</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Data de Nascimento</TableCell>
-                                <TableCell align="center" sx={{ fontWeight: 'bold' }}>Responsável</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {dataState.alunos.map((aluno) => (
-                                <TableRow key={aluno.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
-                                    <TableCell align="center">{aluno.ra}</TableCell>
-                                    <TableCell align="center">{aluno.nome}</TableCell>
-                                    <TableCell align="center">{aluno.laudado ? 'Sim' : 'Não'}</TableCell>
-                                    <TableCell align="center">{aluno.dtNasc}</TableCell>
-                                    <TableCell align="center">{aluno.filiacoes[0]?.responsavel?.nome || 'Não informado'}</TableCell>
+                            </TableHead>
+                            <TableBody>
+                                {dataState.professores.map((professor) => (
+                                    <TableRow key={professor.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
+                                        <TableCell align="center">{professor.nome}</TableCell>
+                                        <TableCell align="center">{professor.cpf}</TableCell>
+                                        <TableCell align="center">{professor.telefone}</TableCell>
+                                        <TableCell align="center">{professor.email}</TableCell>
+                                        <TableCell align="center">{professor.dtNasc}</TableCell>
+                                        <TableCell align="center">
+                                            <EditIcon onClick={() => handleEdit(professor)} style={{ color: 'blue', cursor: 'pointer', marginRight: 8 }} />
+                                            <DeleteIcon onClick={() => handleDelete(professor)} style={{ color: 'red', cursor: 'pointer' }} />
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 1 }}>
+                        <h2 style={{ marginLeft: 16 }}>Alunos</h2>
+                        <Table>
+                            <TableHead>
+                                <TableRow sx={{ backgroundColor: '#e0e0e0' }}>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>RA</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Nome</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Laudado</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Data de Nascimento</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Responsável</TableCell>
+                                    <TableCell align="center" sx={{ fontWeight: 'bold' }}>Ações</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
-        </div>
-        <ModalAtrelar setDataState={setDataState} setOpenModal={setOpenModal} role={role} openModal={openModal} idSala={dataState.id}/>
+                            </TableHead>
+                            <TableBody>
+                                {dataState.alunos.map((aluno) => (
+                                    <TableRow key={aluno.id} hover sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f7f7f7' } }}>
+                                        <TableCell align="center">{aluno.ra}</TableCell>
+                                        <TableCell align="center">{aluno.nome}</TableCell>
+                                        <TableCell align="center">{aluno.laudado ? 'Sim' : 'Não'}</TableCell>
+                                        <TableCell align="center">{aluno.dtNasc}</TableCell>
+                                        <TableCell align="center">{aluno.filiacoes[0]?.responsavel?.nome || 'Não informado'}</TableCell>
+                                        <TableCell align="center">
+                                            <EditIcon onClick={() => handleEdit(aluno)} style={{ color: 'blue', cursor: 'pointer', marginRight: 8 }} />
+                                            <DeleteIcon onClick={() => handleDelete(aluno)} style={{ color: 'red', cursor: 'pointer' }} />
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </div >
+            <ModalAtrelar setDataState={setDataState} setOpenModal={setOpenModal} role={role} openModal={openModal} idSala={dataState.id} />
         </>
     )
 }
