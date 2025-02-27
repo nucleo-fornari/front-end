7d32c60bf5f2595473ce5a2c4e46d934e4276c6a
Conde <157609877+fvconde@users.noreply.github.com>
2025-02-27 14:32:05 -0300

    #feat - feat: implementing responsive for header-bar component

--- nucleo-fornari/frontend/src/components/header-bar/headerBar.jsx
+++ nucleo-fornari/frontend/src/components/header-bar/headerBar.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './headerBar.css'
 
-const HeaderBar = ({title}) => {
+import SingleButtonComponent from "../side-menu/single-button/singleButtonComponent";
+
+import logoutIcon from "../../assets/imgs/logoutIcon.svg";
+
+const HeaderBar = ({ title }) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1024);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
-        <div className={'container-header'}>
+        <div className={`container-header ${isMobile ? 'container-header-mobile' : ''}` }>
             <span className={'title'}>{title}</span>
+            {isMobile && (
+                <div>
+                <SingleButtonComponent
+                    btnIcon={logoutIcon}
+                    btnText={"Sair"}
+                    event={"logout"}
+                />
+                </div>
+            )}
+            
+            
         </div>
     )
 }
