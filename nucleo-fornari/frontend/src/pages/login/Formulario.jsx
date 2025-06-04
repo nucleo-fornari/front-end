656ecf95569cd5b9398508356578a45e5d4f5920
Conde <157609877+fvconde@users.noreply.github.com>
2025-06-03 22:13:44 -0300

    #fix limpando formulario para login

--- nucleo-fornari/frontend/src/pages/login/Formulario.jsx
+++ nucleo-fornari/frontend/src/pages/login/Formulario.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Button, IconButton, InputAdornment, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-// import { useAuth } from "../../AuthProvider";
 import useApi from '../../hooks/ApiHook';
 
 const Formulario = () => {
@@ -13,7 +12,7 @@ const Formulario = () => {
   const [senha, setSenha] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState({ email: '', senha: '' });
-  // const { user, login } = useAuth();
+
   const api = useApi();
 
   const handleLogin = async (event) => {
@@ -40,7 +39,7 @@ const Formulario = () => {
           sessionStorage.ID_SALA = response.data.salaId;
           setTimeout(() => {
             redirectByRole(response.data.funcao);
-          }, 500);
+          }, 300);
         }
         
       }
@@ -61,16 +60,7 @@ const Formulario = () => {
     }
   };
 
-  useEffect(() => {
-    const id = sessionStorage.getItem('ID');
-    const func = sessionStorage.getItem('FUNC');
-
-    if (id && func) {
-      redirectByRole(func);
-    }
-  }, []);
-
-  const redirectByRole = (role) => {
+  const redirectByRole = useCallback((role) => {
     switch (role) {
       case 'RESPONSAVEL':
         navigate('/responsavel');
@@ -85,7 +75,16 @@ const Formulario = () => {
         console.log('Erro ao redirecionar para a rota do user');
         break;
     }
-  };
+  }, [navigate]) ;
+
+  useEffect(() => {
+    const id = sessionStorage.getItem('ID');
+    const func = sessionStorage.getItem('FUNC');
+
+    if (id && func) {
+      redirectByRole(func);
+    }
+  }, [redirectByRole]);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
